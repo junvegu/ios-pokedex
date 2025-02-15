f76c27559c46892ee280f526d02466ff7099cb02
Junior <juniorquegu@gmail.com>
2025-02-15 09:24:26 -0500

    #config: setup init

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Junior Quevedo Gutiérrez  on 15/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
